41e771954a8e4ec813112a3997393f1558e4e305
Chris Hallberg <crhallberg@gmail.com>
2018-01-26 22:37:55 +0000

    Convert object_oriented_collision to p5.js.

---
+++ CodeExamples/object_oriented_collision.js
@@ -0,0 +1,130 @@
+// a single Circle object, controlled by the mouse
+var mouseCircle;
+
+// a list of rectangles
+var rects = new Array(8);
+
+function setup() {
+    var canvas = createCanvas(600, 400);
+    canvas.parent("sketch");
+
+    // create a new Circle with 30px radius
+    mouseCircle = Circle(0, 0, 30);
+
+    // generate rectangles in random locations
+    // but snap to grid!
+    for (var i = 0; i < rects.length; i++) {
+        var x = round(random(50, width - 50) / 50) * 50;
+        var y = round(random(50, height - 50) / 50) * 50;
+        rects[i] = Rectangle(x, y, 50, 50);
+    }
+}
+
+function draw() {
+    background(255);
+
+    // go through all rectangles...
+    var c = mouseCircle.get();
+    for (var i = 0; i < rects.length; i++) {
+        rects[i].checkCollision(c); // check for collision
+        rects[i].display(); // and draw
+    }
+
+    // update circle's position and draw
+    mouseCircle.update();
+    mouseCircle.display();
+}
+
+// Circle class
+function Circle(_x, _y, _r) {
+    return (function() {
+        var x = _x;
+        var y = _y;
+        var r = _r;
+
+        function get() {
+            return {
+                x: x,
+                y: y,
+                r: r,
+            };
+        }
+
+        function update() {
+            x = mouseX;
+            y = mouseY;
+        }
+
+        function display() {
+            fill(0, 150);
+            noStroke();
+            ellipse(x, y, r * 2, r * 2);
+        }
+
+        // make these parts public:
+        return {
+            get: get,
+            update: update,
+            display: display,
+        };
+    })();
+}
+
+// Rectangle class
+function Rectangle(_x, _y, _w, _h) {
+    return (function() {
+        var x = _x;
+        var y = _y;
+        var w = _w;
+        var h = _h;
+        var hit = false;
+
+        // check for collision with the circle using the
+        // Circle/Rect function we made in the beginning
+        function checkCollision(c) {
+            hit = circleRect(c.x, c.y, c.r, x, y, w, h);
+        }
+
+        // draw the rectangle
+        // if hit, change the fill color
+        function display() {
+            if (hit) fill(255, 150, 0);
+            else fill(0, 150, 255);
+            noStroke();
+            rect(x, y, w, h);
+        }
+
+        return {
+            checkCollision: checkCollision,
+            display: display,
+        };
+    })();
+}
+
+// CIRCLE/RECTANGLE
+function circleRect(cx, cy, radius, rx, ry, rw, rh) {
+    // temporary variables to set edges for testing
+    var testX = cx;
+    var testY = cy;
+
+    // which edge is closest?
+    // test left edge
+    if (cx < rx) testX = rx;
+    else if (cx > rx + rw)
+        // right edge
+        testX = rx + rw;
+    // top edge
+    if (cy < ry) testY = ry;
+    else if (cy > ry + rh)
+        // bottom edge
+        testY = ry + rh;
+
+    // get distance from closest edges
+    var distance = dist(cx, cy, testX, testY);
+
+    // if the distance is less than the radius, collision!
+    if (distance <= radius) {
+        return true;
+    }
+    return false;
+}
